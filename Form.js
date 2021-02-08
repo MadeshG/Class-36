1f447e0b049d567d85d24e6dc5c3062662bb49f7
MadeshG <74277922+MadeshG@users.noreply.github.com>
2021-02-07 19:58:56 -0600

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,28 @@
+class Form{
+    constructor(){
+
+    }
+    display(){
+        var title=createElement('h2')
+        title.html("Car Racing")
+        title.position(130,0)
+
+        var input=createInput("name")
+        input.position(130,160)
+
+        var button=createButton('Start')
+        button.position(250,200)
+
+        var greeting=createElement('h3')
+        button.mousePressed(function(){
+            input.hide()
+            button.hide()
+            var name = input.value()
+            playerCount+=1
+            player.update(name)
+            player.updateCount(playerCount)
+            greeting.html("hi"+name)
+            greeting.position(130,150)
+        })
+    }
+}
